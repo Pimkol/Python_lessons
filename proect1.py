dfa326ab5c68c91c25509d0525284211dba2dcc8
Pimkol <150819153+Pimkol@users.noreply.github.com>
2023-11-28 16:43:58 +0600

    Update proect1.py
    исправления в комментаариях

--- proect1.py
+++ proect1.py
@@ -23,6 +23,23 @@ def search(date):
         else:
             print(f'На {date} ничего не запланированно')
 
+''' 
+def search():
+    date=input('Введите дату в формате ДД/ММ/ГГГГ: ')
+    z = 0
+    with open("text.txt", "r") as my_file:
+        for line in my_file:
+            if date in line:
+                z += 1
+                print(line.strip())  # Выводим строку, в которой найдено совпадение
+                break  # Прерываем цикл после первого совпадения
+    for key, value in calendar.items():
+        if key == date or z != 0:
+            print(f'У Вас запланировано событие на {key}: \n{value}')
+        else:
+            print(f'На {date} ничего не запланировано')
+'''
+
 def del_note(date):
     for key in calendar.keys():
         if key==date:
@@ -43,6 +60,18 @@ dat=input('Введите дату в формате ДД/ММ/ГГГГ: ')
 d=datetime.datetime.strptime(dat, "%d/%m/%Y")
 note=input('заметка:\n')
 add_notes(dat,note)
+'''
+ans=int(input('Чтобы добавить еще запись введите 1; \n Для поиска введите 2; \nЧтобы удалить запись введите 3; \n Для просмотра записей введите 4;\n Для выхода из программы нажмите 5;\n'))
+while ans != 5:
+    if ans == 1:
+        search()
+        '''
+        d=datetime.datetime.strptime(dat, "%d/%m/%Y")
+        note=input('заметка:\n')
+        add_notes(dat,note)...'''
+'''
+
+
 ans=int(input('Чтобы добавить еще запись введите 1; \n Для поиска введите 2; \nЧтобы удалить запись введите 3; \n Для просмотра записей введите 4;\n'))
 if ans == 1:
     dat=input('Введите дату в формате ДД/ММ/ГГГГ: ')
