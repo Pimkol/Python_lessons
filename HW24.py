c0e8d3d23bbb2fe0e74786e012b60628d2ff6381
Pimkol <sulovaod@gmail.com>
2023-12-16 15:42:09 +0600

    hw

---
+++ HW24.py
@@ -0,0 +1,31 @@
+import tkinter as tk
+
+
+'''def on_checkbox():
+    if var.get()==1:
+        rez_label.config(text='bu')
+    else:
+        rez_label.config(text='b4u')'''
+
+def show_state():
+    rez_label.config(text=f'опция 1:{var1.get()}, Опция2: {var2.get()}')
+
+root=tk.Tk()
+root.title('hw')
+
+var1=tk.IntVar()
+var2=tk.IntVar()
+
+checkbutt1=tk.Checkbutton(root, text='dfgd',variable=var1, onvalue=1, offvalue=2)#command=on_checkbox)
+checkbutt2=tk.Checkbutton(root, text='dfgd',variable=var2, onvalue=1, offvalue=2)#command=on_checkbox)
+checkbutt1.pack()
+checkbutt2.pack()
+
+show_button=tk.Button(root, text='cостояние', command=show_state)
+show_button.pack()
+
+rez_label=tk.Label(root, text='')
+rez_label.pack()
+
+root.mainloop()
+#какое обучение, такое и дз
