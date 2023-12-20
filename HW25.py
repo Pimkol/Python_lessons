bfc02724fa969adaa1524ab7b9b83db9b77d2dcd
mrchangaridi <40235988+mrchangaridi@users.noreply.github.com>
2023-12-21 00:14:04 +0600

    Update HW25.py

--- HW25.py
+++ HW25.py
@@ -5,13 +5,15 @@ from sqlalchemy.orm import sessionmaker
 
 Base=declarative_base()
 
+# В данном коде допущены опечатки, не похоже на Вас, подправил и отметил
+
 class User(Base):
     __tablename__='user'
-    id=Column (Integer, Sequence('user_id_seq'), prinary_key=True)
+    id=Column (Integer, Sequence('user_id_seq'), primary_key=True) #Здесь была в primary key
     name=Column(String(50))
     age=Column(Integer)
 
-engine=create_engine('aqlite:///:memory:', echo=True)
+engine=create_engine('sqlite:///:memory:', echo=True) #Тут было не sqlite, а aqlite
 Base.metadata.create_all(bind=engine)
 Session=sessionmaker(bind=engine)
 session=Session()
@@ -19,7 +21,7 @@ session=Session()
 person1=User(name='Python', age=50)
 person2=User(name='Java', age=60)
 
-session.add_all(person1,person2)
+session.add_all([person1,person2]) # Здесь советую добавлять не аргументами "(person1,person2)", а списком объектов "([person1,person2])"
 session.commit()
 
 persons=session.query(User).all()
