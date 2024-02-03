d11f881277973175d94dc56aa1fc643cc1d91f86
Pimkol <suslovaod@gmail.com>
2024-02-03 14:00:29 +0600

    HW33

---
+++ HW33.py
@@ -0,0 +1,13 @@
+import numpy as np 
+array1= np.array([[1,2,3],[4,5,6]])
+array2= np.array([2,4,6])
+result = array1*array2
+print (result)
+
+stepen = array1**array2
+print (stepen)
+
+delen = array1/5
+delen2 = array2/5
+print (delen)
+print(delen2)
